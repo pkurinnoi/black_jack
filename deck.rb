d70f2f93fc412da79103b40205c0328c4287e5f5
Pavel <p.kurinnoi@gmail.com>
2023-02-02 18:12:56 +0300

    pk0-deck_class

---
+++ deck.rb
@@ -0,0 +1,20 @@
+class Deck
+
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  SUITS = ['+', '<3', '^', '<>']
+  COSTS = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => [1, 11]}
+
+  def initialize
+    @deck = []
+
+    SUITS.each do |suit|
+      VALUES.each do|value|
+        cost = COSTS[value]
+        playcard = Playcard.new(value, suit, cost)
+        @deck << playcard
+      end
+    end
+    puts "New deck with #{@deck.length} cards successfully created!"
+  end
+end
+
