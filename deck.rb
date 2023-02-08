0658cfb727e1ff051356eb756ed7cd014851ebbc
Pavel <p.kurinnoi@gmail.com>
2023-02-08 16:14:04 +0300

    pk0-cons_from_playcard_upd

--- deck.rb
+++ deck.rb
@@ -1,7 +1,7 @@
 class Deck
 
-  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  SUITS = ['+', '<3', '^', '<>']
+  VALUES = Playcard::VALUES
+  SUITS = Playcard::SUITES
   COSTS = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
 
   attr_reader :deck
