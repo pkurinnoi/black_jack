df9f44bebd301772203dac6b18b0b0b564be33b0
Pavel <p.kurinnoi@gmail.com>
2023-02-09 12:12:36 +0300

    pk0-cons_from_playcard_upd

--- deck.rb
+++ deck.rb
@@ -1,7 +1,5 @@
 class Deck
 
-  VALUES = Playcard::VALUES
-  SUITS = Playcard::SUITES
   COSTS = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
 
   attr_reader :deck
@@ -9,8 +7,8 @@ class Deck
   def initialize
     @deck = []
 
-    SUITS.each do |suit|
-      VALUES.each do|value|
+    Playcard::SUITES.each do |suit|
+      Playcard::VALUES.each do|value|
         cost = COSTS[value]
         playcard = Playcard.new(value, suit, cost)
         @deck << playcard
